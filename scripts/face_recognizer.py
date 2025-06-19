f632a58193fb036583a83acb773919f05437b715
Nika <131802365+ol1vvvka@users.noreply.github.com>
2025-06-19 14:58:19 +0500

    Добавлены уточняющие комментарии к частям кода

--- scripts/face_recognizer.py
+++ scripts/face_recognizer.py
@@ -44,26 +44,36 @@ cv2.namedWindow("Face Recognition", cv2.WINDOW_NORMAL)
 messagebox.showinfo("Распознавание", "Распознавание запущено. Нажмите ESC для выхода.")
 
 while True:
+     # считывание кадра с камеры
     ret, frame = cap.read()
+    # если не удалось - предупреждаем пользователя
     if not ret:
         messagebox.showwarning("Предупреждение", "Не удалось считать кадр.")
         continue
 
+    # преобразование изображения в оттенки серого
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # находим лица на изображении
     faces = detector.detectMultiScale(gray, 1.3, 5)
 
     for (x, y, w, h) in faces:
+        # получение области лица
         face = gray[y:y+h, x:x+w]
+        # предсказание метки (id) и уровеня уверенности - 30-60 в пределах нормы, выше - не найден
         label, confidence = model.predict(face)
+        # получение имени по метке
         name = labels.get(label, "Unknown")
 
+        # формирование текста с именем и уверенностью
         text = f"{name} ({confidence:.1f})"
+        # отображение имени и рамки вокруг лица
         cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     # Показываем изображение в окне с новым названием
     cv2.imshow("Face Recognition", frame)
-    
+
+    # прерывание цикла по нажатию клавиши ESC
     if cv2.waitKey(1) == 27:  # ESC
         break
 
